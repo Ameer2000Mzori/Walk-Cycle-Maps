f06da173151c4207fe5ae3cd3f3698fb0ac4ebda
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-01-22 11:07:50 +0100

    added logic where you ask user for his location

--- src/api.js
+++ src/api.js
@@ -1,12 +1,31 @@
 console.log('hallo world')
-var map = L.map('map').setView([51.505, -0.09], 13)
+
+// getting the user location
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      console.log(position.coords)
+      const { latitude, longitude } = position.coords
+      console.log(`This is your position: ${latitude}, ${longitude}`)
+    },
+    () => {
+      console.log('There is no location')
+    }
+  )
+}
+
+//
+var map = L.map('map').setView([52.3624756, 5.2083721, 21], 13)
+
+// my location
+// 52.3624756,5.2083721,21
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map)
 
-L.marker([51.5, -0.09])
+L.marker([52.3624756, 5.2083721, 21])
   .addTo(map)
   .bindPopup('A pretty CSS popup.<br> Easily customizable.')
   .openPopup()
