551c82331dec90c61d235f300d7dd525a139a56f
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-01-22 11:49:06 +0100

    small changes

--- src/addMarkersLogic.js
+++ src/addMarkersLogic.js
@@ -1,24 +1,24 @@
 // my location
 // 52.3624756,5.2083721,21
 
-// getting the user location
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    () => {
-      //   map = L.map('map').setView([52.3624756, 5.2083721, 21, 21], 13)
+// // getting the user location
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(
+//     () => {
+//       var map = L.map('map').setView([52.3624756, 5.2083721, 21, 21], 13)
 
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map)
+//       L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//         attribution:
+//           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//       }).addTo(map)
 
-      L.marker([52.3624756, 5.2083721, 21])
-        .addTo(map)
-        .bindPopup('Your current location <br> (this is added marker)')
-        .openPopup()
-    },
-    () => {
-      console.log('There is no location')
-    }
-  )
-}
+//       L.marker([52.3624756, 5.2083721, 21])
+//         .addTo(map)
+//         .bindPopup('Your current location <br> (this is added marker)')
+//         .openPopup()
+//     },
+//     () => {
+//       console.log('There is no location')
+//     }
+//   )
+// }
