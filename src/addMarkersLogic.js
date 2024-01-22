7152172ef96f2953f6e624e193f4b7c5241a496b
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-01-22 11:41:18 +0100

    file orginization, styling and more fixes

--- src/addMarkersLogic.js
+++ src/addMarkersLogic.js
@@ -1,31 +1,24 @@
-console.log('hallo world')
+// my location
+// 52.3624756,5.2083721,21
 
-// // getting the user location
-// if (navigator.geolocation) {
-//   navigator.geolocation.getCurrentPosition(
-//     (position) => {
-//       console.log(position.coords)
-//       const { latitude, longitude } = position.coords
-//       console.log(`This is your position: ${latitude}, ${longitude}`)
-//     },
-//     () => {
-//       console.log('There is no location')
-//     }
-//   )
-// }
+// getting the user location
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    () => {
+      //   map = L.map('map').setView([52.3624756, 5.2083721, 21, 21], 13)
 
-// //
-// var map = L.map('map').setView([latitude, longitude, 21], 13)
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map)
 
-// // my location
-// // 52.3624756,5.2083721,21
-
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//   attribution:
-//     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-// }).addTo(map)
-
-// L.marker([latitude, longitude, 21])
-//   .addTo(map)
-//   .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-//   .openPopup()
+      L.marker([52.3624756, 5.2083721, 21])
+        .addTo(map)
+        .bindPopup('Your current location <br> (this is added marker)')
+        .openPopup()
+    },
+    () => {
+      console.log('There is no location')
+    }
+  )
+}
